7802db26eafae8fff5569548800ab88d6b3e717d
electrikbox <electrikbox@icloud.com>
2024-02-05 21:38:10 +0100

    refact js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,36 +1,38 @@
 $(document).ready(() => {
-    const amenityIds = {};
-    $('input[type="checkbox"]').on('change', function () {
+  const amenityIds = {};
+
+  $('input[type="checkbox"]').on('change', function () {
       const amenityId = $(this).data('id');
       const amenityName = $(this).data('name');
 
-      if ($(this).is(':checked')) {
-        amenityIds[amenityId] = amenityName;
+      if (this.checked) {
+          amenityIds[amenityId] = amenityName;
       } else {
-        delete amenityIds[amenityId];
+          delete amenityIds[amenityId];
       }
 
       let amenityList = Object.values(amenityIds).join(', ');
       const maxLength = 37;
+
       if (amenityList.length > maxLength) {
-        amenityList = `${amenityList.substring(0, maxLength)}...`;
+          amenityList = `${amenityList.substring(0, maxLength)}...`;
       }
+
       $('.amenities h4').text(amenityList);
-    });
   });
 
-  $(document).ready(() => {
-    $.ajax({
-        url: 'http://0.0.0.0:5001/api/v1/status/',
-        success(response) {
-            if (response.status === 'OK') {
-                $('#api_status').addClass('available');
-            } else {
-                $('#api_status').removeClass('available');
-            }
-        },
-        error(error) {
-            console.error('Error fetching API status', error);
-        },
-    });
+  $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/status/',
+      success(response) {
+          const apiStatus = $('#api_status');
+          if (response.status === 'OK') {
+              apiStatus.addClass('available');
+          } else {
+              apiStatus.removeClass('available');
+          }
+      },
+      error(error) {
+          console.error('Error fetching API status', error);
+      },
   });
+});
