d11c117f1a7ba5fe3fd3f2775bcc278f54dabbbe
electrikbox <electrikbox@icloud.com>
2024-02-05 23:42:11 +0100

    fix indent

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -2,31 +2,31 @@ $(document).ready(() => {
   const amenityIds = {};
 
   $('input[type="checkbox"]').on('change', function () {
-      const amenityId = $(this).data('id');
-      const amenityName = $(this).data('name');
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
 
-      if (this.checked) {
-          amenityIds[amenityId] = amenityName;
-      } else {
-          delete amenityIds[amenityId];
-      }
+    if (this.checked) {
+      amenityIds[amenityId] = amenityName;
+    } else {
+      delete amenityIds[amenityId];
+    }
 
-      let amenityList = Object.values(amenityIds).join(', ');
-      const maxLength = 37;
+    let amenityList = Object.values(amenityIds).join(', ');
+    const maxLength = 37;
 
-      if (amenityList.length > maxLength) {
-          amenityList = `${amenityList.substring(0, maxLength)}...`;
-      }
+    if (amenityList.length > maxLength) {
+      amenityList = `${amenityList.substring(0, maxLength)}...`;
+    }
 
-      $('.amenities h4').text(amenityList);
+    $('.amenities h4').text(amenityList);
   });
 
   $.get('http://0.0.0.0:5001/api/v1/status/')
     .then(response => {
-        const apiStatus = $('#api_status');
-        apiStatus.toggleClass('available', response.status === 'OK');
+      const apiStatus = $('#api_status');
+      apiStatus.toggleClass('available', response.status === 'OK');
     })
     .catch(error => {
-        console.error('Error fetching API status', error);
+      console.error('Error fetching API status', error);
     });
 });
